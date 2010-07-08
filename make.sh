cea137a8ea3f0b4eef5ce85d66fdd24c7637b8bf
Joel Lehtonen <joel.lehtonen@iki.fi>
2010-07-08 13:08:14 +0300

    SyncTeXiä ja kirjaladontaa varten vähän temppuja.

--- make.sh
+++ make.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -eu
-OUTFILE=lehtonen_siljander_pro_gradu.pdf
+OUTFILE=lehtonen_siljander_pro_gradu
 pdflatex rakenne.tex
 bibtex rakenne
 pdflatex rakenne.tex
-pdflatex rakenne.tex
-mv rakenne.pdf "$OUTFILE"
+pdflatex -synctex=1 rakenne.tex
+mv rakenne.pdf "$OUTFILE.pdf"
+mv rakenne.synctex.gz "$OUTFILE.synctex.gz"
 
 echo
 echo 'Gradu käännetty!'
