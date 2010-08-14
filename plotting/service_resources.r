686a8be6cee1412f21fcbb9e38183c1d9218c979
Joel Lehtonen <joel.lehtonen@iki.fi>
2010-08-14 17:57:52 +0300

    Tuomolta saadut anomalialistat, päivitys palvelun kuvasta. R-skriptejä.

--- plotting/service_resources.r
+++ plotting/service_resources.r
@@ -1,6 +1,9 @@
-pdf("../pics/service_resources.pdf")
+# Plots service resource plot. Modified manually then.
+
+svg("../pics/service_resources-orig.svg")
 
 palvelu <- read.csv("../anomaliat/a.csv",col.names=c("resource","count","paramCount","sd","grams"),header=F)
-plot(palvelu$grams,palvelu$paramCount,main="Palvelun resurssien ominaisuudet",xlab="N-grammien lukumäärä",ylab="GET-parametrien lukumäärä")
+plot(palvelu$grams,palvelu$paramCount,xlab="N-grammien lukumäärä",ylab="GET-parametrien lukumäärä")
+text(palvelu$grams,palvelu$paramCount,palvelu$resource,srt="0")
 
 dev.off()
