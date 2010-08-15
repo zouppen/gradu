233b37527da9075c01bc60290993397963fee504
Joel Lehtonen <joel.lehtonen@iki.fi>
2010-08-15 16:23:50 +0300

    Piirrettiin kuvaajat uudestaan. Lisättiin myös diffuusiokuvat.
    Kuvaajista poistettiin otsikot ja lisättiin n sekä käytetään nyt
    päätteellistä fonttia.  Diffuusiokarttojen piirrossa käytetään
    täristystä! :-O

--- plotting/service_resources.r
+++ plotting/service_resources.r
@@ -1,6 +1,7 @@
 # Plots service resource plot. Modified manually then.
 
 svg("../pics/service_resources-orig.svg")
+par(family="serif")
 
 palvelu <- read.csv("../anomaliat/a.csv",col.names=c("resource","count","paramCount","sd","grams"),header=F)
 plot(palvelu$grams,palvelu$paramCount,xlab="N-grammien lukumäärä",ylab="GET-parametrien lukumäärä")
